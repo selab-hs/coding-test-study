141ed37c45ebfb0508047664ac84e0bf2bee622f
InJun2 <os2864@naver.com>
2023-02-27 20:05:05 +0900

    add content

---
+++ programmers/src/Greedy/단속카메라/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ programmers/src/Greedy/체육복/Solution.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
+        return answer;
+    }
+}
