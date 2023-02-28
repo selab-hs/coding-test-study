ea02870bfdb8c4bc42adb18c5cc3e1d4c6b2007b
InJun2 <os2864@naver.com>
2023-02-28 20:15:47 +0900

    지금까지 진행 코딩테스트 작성

--- programmers/src/4-Sort/2-가장큰수/Solution.java
+++ programmers/src/4-Sort/2-가장큰수/Solution.java
@@ -1,6 +1,26 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
     public String solution(int[] numbers) {
         String answer = "";
+        String[] nums = new String[numbers.length];
+
+        for(int i = 0; i < numbers.length; i++){
+            nums[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(nums, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return (o2+o1).compareTo(o1+o2);
+            }
+        });
+
+        if (nums[0].equals("0")) return "0";
+
+        for(String s: nums) answer += s;
+
         return answer;
     }
 }
