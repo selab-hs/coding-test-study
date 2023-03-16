686fcb1ba8359461e8f0dd34acd887a414d3aa4e
황인준 <50690859+InJun2@users.noreply.github.com>
2023-03-17 02:52:49 +0900

    Update Solution.java

--- programmers/src/7-DynamicProgramming/3-N으로표현/Solution.java
+++ programmers/src/7-DynamicProgramming/3-N으로표현/Solution.java
@@ -1,6 +1,53 @@
+import java.util.HashSet;
+import java.util.Set;
+
 class Solution {
     public int solution(int N, int number) {
-        int answer = 0;
-        return answer;
+        Set<Integer> count = new HashSet<>();
+        Set<Integer> count2 = new HashSet<>();
+        int cnt = 1;
+
+        count.add(N);
+        count2.add(N);
+
+        for(int i=1; i<9; i++) {
+            for(int a : count) {
+                if(number == a) {
+                    return cnt;
+                }
+
+                count2.add(plus(a, N));
+                count2.add(minus(a, N));
+                count2.add(multiplication(a, N));
+                count2.add(division(a, N));
+                count2.add(a * 10 + N);
+            }
+
+            for(int a2 : count2) {
+                count.add(a2);
+            }
+            cnt ++;
+        }
+
+        return -1;
     }
+
+    private int plus(int a, int b) {
+        return a + b;
+    }
+
+    private int minus(int a, int b) {
+        return a - b;
+    }
+
+    private int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    private int division(int a, int b) {
+        if(a != 0 && b!= 0)
+            return a / b;
+        return a;
+    }
+
 }
